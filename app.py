a70139ead32bbec49c3603f7b5c231e25b6461f1
Sree Varsha <89973121+31032001@users.noreply.github.com>
2022-03-20 22:17:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,30 @@
+import sklearn.datasets
 import streamlit as st
-from PIL import Image
+st.title("Iris Classifier- API")
 
-st.title("Iris Species Classifier")
-
-sl = st.slider('SEPAL LENGTH', 4.3, 7.9, 0.5)
-sw = st.slider('SEPAL WIDTH', 2.0, 4.4, 0.5)
-pl = st.slider('PETAL LENGTH', 1.0, 6.9, 0.5)
-pw = st.slider('PETAL WIDTH', 0.1,2.5, 0.5)
+sl = st.slider('Sepal Length', 4.3, 7.9, 0.5)
+sw = st.slider('Sepal Width', 2.0, 4.4, 0.5)
+pl = st.slider('Petal Length', 1.0, 6.9, 0.5)
+pw = st.slider('Petal Width', 0.1,2.5, 0.5)
 
 from sklearn.datasets import load_iris
+
 iris = load_iris()
+
+iris.data
+
+iris.target
 
 from sklearn.cluster import KMeans
 
 kmeans = KMeans(n_clusters=3)
 
 model = kmeans.fit(iris.data)
-op = model.predict([[sl,sw,pl,pw]])
-op = iris.target_names[op[0]]
 
+model.labels_
 
-image1 = Image.open('Images/setosa.jpeg')
-image2 = Image.open('Images/versicolor.jpeg')
-image3 = Image.open('Images/virginica.jpeg')
+model.cluster_centers_
 
-if op[0] == 0: st.image(image1)
-elif op[0] == 1: st.image(image2)
-else: st.image(image3)
+result = model.predict([[sl,sw,pl,pw]])
+result = iris.target_names[result[0]]
+st.title(result)
